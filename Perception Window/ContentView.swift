1db3a2d7c4569c147cdda002a8b8f55c9b34dc3e
Agajohn <rn_Esl@yahoo.com>
2026-08-03 14:04:17 -0700

    Initial Commit

---
+++ Perception Window/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Perception Window
+//
+//  Created by Aren Agajohn on 8/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Perception Window/Perception_WindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Perception_WindowApp.swift
+//  Perception Window
+//
+//  Created by Aren Agajohn on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Perception_WindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
